6d2b66bea5f7c0401215f8f9a8dd4be985594fe4
Ananda Krishnan P S <anandakrishnan2125@gmail.com>
2024-02-07 17:14:20 +0530

    added a CustomUser model

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)  # Add username field
+    password = models.CharField(max_length=128)
+
+    groups = models.ManyToManyField('auth.Group', related_name='custom_users')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_users')
+
+    def __str__(self):
+        return self.username
